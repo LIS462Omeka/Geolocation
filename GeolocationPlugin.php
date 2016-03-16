91b147c02542403e9ef02d861a4f5380a570c293
jbolmarcich <jbolmarcich@gmail.com>
2016-03-16 16:07:57 -0400

    Update GeolocationPlugin.php

--- GeolocationPlugin.php
+++ GeolocationPlugin.php
@@ -288,7 +288,7 @@ class GeolocationPlugin extends Omeka_Plugin_AbstractPlugin
     {
         queue_css_file('geolocation-marker');
         queue_js_url('//maps.google.com/maps/api/js');
-        queue_js_file('map');.
+        queue_js_file('map');
         queue_js_url('//stamen-maps.a.ssl.fastly.net/js/');
         queue_js_file('tile.stamen');
     }
